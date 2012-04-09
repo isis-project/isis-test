291fbcc9a6247c61f76db0f7ab415a4995fab963
Stuart Eichert <stuart.eichert@palm.com>
2012-04-09 16:01:18 -0700

    Add enyoPath and debug argument when launching the browser
    This will make sure that enyo knows its correct path on the
    desktop and will use 'debug' mode.

--- main.cpp
+++ main.cpp
@@ -12,11 +12,11 @@ int main(int argc, char *argv[])
     view.resize(1024, 768);
     view.setStyleSheet("background-color:rgb(150,147,88); padding: 7px ; color:rgb(255,255,255)");
     view.page()->settings()->setAttribute(QWebSettings::PluginsEnabled, true);
-    QString browserAppUrl = QString("file://")+QDir::currentPath()+QString("/../isis-browser/index.html");
+    QString browserAppUrl = QString("file://")+QDir::currentPath()+QString("/../isis-browser/index.html?enyoPath=../enyo-1.0/framework&debug");
 
     QStringList args = a.arguments();
     if (args.count() > 1)
-        browserAppUrl += QString("?url=") + args.last();
+        browserAppUrl += QString("&url=") + args.last();
 
     view.load(QUrl(browserAppUrl));
     view.show();
